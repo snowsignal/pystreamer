2b57f0b06662d45dcfb45fa98fb892e31e935f7d
Jackson Lewis <st.japa6@gmail.com>
2018-01-05 17:15:13 -0800

    Set up for testing on raspberry pi

--- run.py
+++ run.py
@@ -1,27 +1,31 @@
 import picamera
 import socket
+import io
 
 # Code for CitySynth project
 #
 # Let's hope this works. If it's successful, yay. If not, we're screwed. Probably.
 #
 frames = 0
-
+'''
 print("Connecting to socket.")
 socket_stream = socket.socket() 
-socket_stream.connect(('citysynth_camera_server',8000))
+socket_stream.connect(('localhost',8000))
 sconnect = socket_stream.makefile('wb')
 print("Connection has finished")
-
+'''
 class Camera:
     def __init__(self):
           self.camera = picamera.PiCamera()
           self.camera.resolution = (1080,768)
           self.camera.start_preview()
     def begin_stream(self):
+	global frames
         stream = io.BytesIO()
-        for i in self.camera.capture_continous(stream,'jpeg'):
-
+        for i in self.camera.capture_continuous(stream,'jpeg'):
+		frames += 1
+		print(frames)
+'''
             # Write the length of the capture to the stream and flush to
             # ensure it actually gets sent
 
@@ -38,6 +42,6 @@ class Camera:
             stream.seek(0)
             stream.truncate()
             connection.write(struct.pack('<L', 0))
-
+'''
 cam = Camera()
 cam.begin_stream()
