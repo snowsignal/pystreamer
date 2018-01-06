8024b00231b0c3e3fc4673a58cf2258649ad3974
Jackson Lewis <st.japa6@gmail.com>
2018-01-05 16:46:17 -0800

    Initial script commit

---
+++ run.py
@@ -0,0 +1,43 @@
+import picamera
+import socket
+
+# Code for CitySynth project
+#
+# Let's hope this works. If it's successful, yay. If not, we're screwed. Probably.
+#
+frames = 0
+
+print("Connecting to socket.")
+socket_stream = socket.socket() 
+socket_stream.connect(('citysynth_camera_server',8000))
+sconnect = socket_stream.makefile('wb')
+print("Connection has finished")
+
+class Camera:
+    def __init__(self):
+          self.camera = picamera.PiCamera()
+          self.camera.resolution = (1080,768)
+          self.camera.start_preview()
+    def begin_stream(self):
+        stream = io.BytesIO()
+        for i in self.camera.capture_continous(stream,'jpeg'):
+
+            # Write the length of the capture to the stream and flush to
+            # ensure it actually gets sent
+
+            sconnect.write(struct.pack('<L', stream.tell()))
+            sconnect.flush()
+
+            # Rewind the stream and send the image data over the wire
+
+            stream.seek(0)
+            sconnect.write(stream.read())
+
+            # Reset the stream for the next capture
+
+            stream.seek(0)
+            stream.truncate()
+            connection.write(struct.pack('<L', 0))
+
+cam = Camera()
+cam.begin_stream()
